1bb225aff680f4c50ea8068650cb75774ad74fb5
Girish Sontakke <girishsontakke7@gmail.com>
2021-08-10 00:05:01 +0530

    initial commit

---
+++ src/components/hero/hero.component.jsx
@@ -0,0 +1,38 @@
+import "./hero.styles.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
+
+function Hero() {
+  const myElement = useRef();
+  useEffect(() => {
+    const skills = [
+      "Web Developer",
+      "ReactJs Developer",
+      "Django Developer",
+      "NodeJs Developer",
+      "SQL Developer",
+      "Open Source Developer",
+    ];
+    init(myElement.current, { showCursor: true, strings: skills });
+  }, []);
+  return (
+    <div className="hero">
+      <div className="hero__left">
+        <p className="hero__left__greet">HELLO WORLD, I AM</p>
+        <h1 className="hero__left__name">GIRISH SONTAKKE, </h1>
+        <h2 className="typewriter">
+          <span ref={myElement}></span>
+        </h2>
+      </div>
+      <div className="hero__right">
+        <img
+          src="./assets/images/hero-section.svg"
+          alt="owner"
+          className="hero__right__image"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
